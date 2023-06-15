1bb1eafa13c1f86ccf55a96c2ea75d2a8f279998
yossi12345 <yossihingel134@gmail.com>
2023-06-15 11:27:25 +0300

    first111

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useMemo, useState } from 'react';
+import './App.scss';
 function App() {
+  const [clock,setClock]=useState(new Date())
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setClock(new Date())
+    },1000)
+    return ()=>{
+      clearInterval()
+    }
+  },[])
+  const clockShow=useMemo(()=>{
+    const seconds=clock.getSeconds()
+    const minutes=clock.getMinutes()
+    const hour=+clock.getHours()
+    return (hour>=10?hour:"0"+hour)+":"+
+    (minutes>=10?minutes:"0"+minutes)+":"+
+    (seconds>=10?seconds:"0"+seconds) 
+  },[clock])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center'>
+      {clockShow}
     </div>
   );
 }
